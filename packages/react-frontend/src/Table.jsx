ff844277dde92b9fa689a9f634c788c4f3ea7fec
Logan Costello <logancostello2022@gmail.com>
2024-10-14 21:25:43 -0700

    table shows id

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -16,6 +16,7 @@ function TableHeader() {
     return (
       <thead>
         <tr>
+          <th>Id</th>
           <th>Name</th>
           <th>Job</th>
           <th>Remove?</th>
@@ -28,6 +29,7 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
+        <td>{row.id}</td>
         <td>{row.name}</td>
         <td>{row.job}</td>
         <td>
